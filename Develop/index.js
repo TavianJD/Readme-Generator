1e14b1799b4fcdee9cebe82d58bdd54a4b65d4ce
hatetavian <tavianjdawson123@gmail.com>
2021-12-11 12:24:42 -0600

    set up the look

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,68 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown")
+const inquirer = require('inquirer')
+const fs = require("fs")
+const path = require("path")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: 'input',
+        name: "Project",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Give a description of your project."
+    },
+    {
+        type: "input",
+        name: "Install",
+        message: "What are the steps required to install your project?"
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "What does user need to know before using application?"
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "Any collaborators, if so list them."
+    },
+    {
+        type: "list",
+        name: "licenses",
+        message: "Please choose a license for your project.",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+ return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+inquirer.prompt(questions) 
+
+.then((userInput) =>{
+    writeToFile("Readme.md", generateMarkdown({...userInput}))
+})
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "None"){
+    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== "None"){
+    return `\n *[License](#license)\n`
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !== "None"){
+    return `## License
+    
+      This project is licensed under the ${license} license`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log("GENERATE MARKDOWN DATA", data)
+  return `# ${data.Project}
+  ${renderLicenseBadge(data.licenses)}
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+${renderLicenseLink(data.licenses)}
+
+
+## Installation
+
+${data.Install}
+
+## Usage
+
+${data.Usage}
+
+
+
+${renderLicenseSection(data.licenses)}
 
 `;
 }
